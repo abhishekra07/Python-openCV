c33d4c8f69c7946494b55c8eda0066f59808f243
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-04-24 21:30:22 +0530

    Updated file to read video through webcam

--- ReadVideoOpenCV.py
+++ ReadVideoOpenCV.py
@@ -1,6 +1,10 @@
 import cv2  # importing computer vision module
 
-cap = cv2.VideoCapture("Resource/big_buck_bunny.mp4")  # reading video
+# cap = cv2.VideoCapture("Resource/big_buck_bunny.mp4")  # reading video
+cap = cv2.VideoCapture(0)  # reading video from webcam. just give id of webcam
+cap.set(3, 640)  # width
+cap.set(4, 480)  # height
+cap.set(10, 100)  # brightness
 
 while True:
     success, img = cap.read()  # read image as video is nothing but sequence/frame of images. success flag indicate
