d7126c3d5cb34e51efaed034a5e1c20fecc8d562
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-04-24 22:42:04 +0530

    Create warpPerspective.py

---
+++ warpPerspective.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resource/kingCard.jfif")
+
+width, height = 250, 350
+
+pts1 = np.float32([[132, 25], [234, 30], [122, 181], [223, 188]])
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+print(img.shape)
+
+cv2.imshow("Image", img)
+cv2.imshow("Output Image", imgOutput)
+
+cv2.waitKey(0)
